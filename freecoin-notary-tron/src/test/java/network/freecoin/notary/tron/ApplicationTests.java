2f84c9aafd97e52ccc74c3b3155a61fbd3203530
blockchainapper <dapp.savior@gmail.com>
2020-09-01 01:52:42 +0800

    init commit

---
+++ freecoin-notary-tron/src/test/java/network/freecoin/notary/tron/ApplicationTests.java
@@ -0,0 +1,13 @@
+package network.freecoin.notary.tron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
