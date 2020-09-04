295aed7853bb5eb9da78bc3ac4cc3b7fa8747340
Peng <pyx5814127@gmail.com>
2020-09-04 09:13:37 +0800

    add entity

--- freecoin-notary-core/src/main/java/network/freecoin/notary/core/dao/mapper/MintStateMapper.java
+++ freecoin-notary-core/src/main/java/network/freecoin/notary/core/dao/mapper/MintStateMapper.java
@@ -1,8 +1,16 @@
 package network.freecoin.notary.core.dao.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import network.freecoin.notary.core.dao.entity.MintState;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author pengyuxiang
  * @date 2020/9/4
  */
-public class MintStateMapper {
+
+@Mapper
+@Repository
+public interface MintStateMapper extends BaseMapper<MintState> {
 }
